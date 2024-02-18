d298280fde448ca33b975652662aafa9c9e36319
melisagoogle <melisagencer@googlemail.com>
2024-02-18 00:00:27 +0000

    i have worked on planet1

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -4,9 +4,17 @@ public class SolarSystem {
         solar.Stars = "100";
         solar.Moon = "4";
         solar.Planets = "2";
+
         FeatureSun xros=new FeatureSun();
         xros.Color="pink";
         xros.temp="2324324324324k";
         xros.Size="23243243243243243234miles";
+
+
+        Planet1 frog=new Planet1();
+        frog.color="blue";
+        frog.temp="-123213124k";
+        frog.size="2km";
+
     }
     }
